dc1515e1286659bd151923e4e3f64239ad436265
linuxic <elennagornova@gmail.com>
2022-03-16 03:43:30 +0300

    fix test

--- internal/controller/handlers.go
+++ internal/controller/handlers.go
@@ -93,7 +93,8 @@ func GetUserOrders(ctx context.Context, service *gophermart.Gophermart) http.Han
 		}
 		var orders Openapi.GetUserOrdersResponse
 		for _, order := range userOrders {
-			orders = append(orders, Openapi.Order{Accrual: &order.Accrual, Number: order.OrderID, Status: order.Status, UploadedAt: order.UploadedAt.Format(time.RFC3339)})
+			accural := float32(order.Accrual)
+			orders = append(orders, Openapi.Order{Accrual: &accural, Number: order.OrderID, Status: order.Status, UploadedAt: order.UploadedAt.Format(time.RFC3339)})
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
